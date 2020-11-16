bac6e196552f51fcb79132b92757dc855048dda2
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-11-16 11:46:41 +0300

    conentct with mongo db and starter nodemon server

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-const app = espress();
+async function start(){
+    try{
+        await mongoose.connect('mongodb+srv://nodejsapp:A123456a@app1.iraug.mongodb.net/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true,
+        });
+        app.listen(PORT, () => {
+            console.log("Server has been started...");
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
