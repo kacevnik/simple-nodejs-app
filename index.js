54423a629b051dc67d5a285d6339d3d2c0928ef2
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-11-16 13:07:06 +0300

    Added routers and express handlebars

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos')
+
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(todoRoutes);
 
 async function start(){
     try{
