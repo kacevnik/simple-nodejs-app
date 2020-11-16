33743e6b26e258df1531765a1acb58eeb0ec7567
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-11-16 22:25:41 +0300

    added eact todos in index page

--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,23 @@
 const {Router} = require('express');
+const Todo = require('../models/todo');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get('/', async (req, res) => {
+
+    const todos = await Todo.find({});
+
+    res.render('index', {
+        title: 'Todo list',
+        isIndex: true,
+        todos,
+    });
 });
 
 router.get('/create', (req, res) => {
-    res.render('create');
+    res.render('create', {
+        title: 'Create todo',
+        isCreate: true,
+    });
 })
 
 module.exports = router;
