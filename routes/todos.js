fc4bb1cc5880b6de2bc56e414ba4a6834dfc6395
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-11-17 20:13:59 +0300

    added seving todo and finish app

--- routes/todos.js
+++ routes/todos.js
@@ -29,4 +29,13 @@ router.post('/create', async (req, res) => {
     res.redirect('/');
 });
 
+router.post('/complete', async (req, res) => {
+    const todo = await Todo.findById(req.body.id);
+
+    todo.complited = !!req.body.complited;
+
+    await todo.save();
+    res.redirect('/');
+});
+
 module.exports = router;
